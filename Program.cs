ed95a0c72a99d8009c1ae8e5debdbb83dc5e59c0
CLux <cplux7@gmail.com>
2019-05-14 10:27:47 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello world";
+
+            Console.WriteLine(message);
+
+
+
+
+        
+        }
+    }
+}
